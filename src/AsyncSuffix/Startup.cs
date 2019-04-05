4e6d40975ab77e40ce0597a4ebb539ae479dfb78
wk-j <somnuk.wk@gmail.com>
2019-04-06 01:23:14 +0700

    Fixed typo

--- src/AsyncSuffix/Startup.cs
+++ src/AsyncSuffix/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace AsyncSubffix {
+namespace AsyncSuffix {
     public class Startup {
         public Startup(IConfiguration configuration) {
             Configuration = configuration;
